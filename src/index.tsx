df2038e1b75bade403c4dc9c4f61eb0e66061c0c
sirgrey <sirgrey@naver.com>
2022-03-05 16:43:23 +0900

    기본 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,15 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
 import { store } from './app/store';
 import { Provider } from 'react-redux';
 import * as serviceWorker from './serviceWorker';
+import HubPage from './pages/HubPage';
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <HubPage />
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
---
+++ src/pages/HubPage/index.tsx
@@ -0,0 +1,10 @@
+import styles from "./style.module.scss"
+
+
+const HubPage = () => {
+return (
+  <div className={styles.hubPage}>
+  </div>);
+}
+
+export default HubPage;
